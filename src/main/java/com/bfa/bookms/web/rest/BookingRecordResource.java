bd6378a65de60a15db6ff97858906bcca74cffad
admin <admin@test.com>
2017-01-24 18:34:51 +0530

    compiled

--- src/main/java/com/bfa/bookms/web/rest/BookingRecordResource.java
+++ src/main/java/com/bfa/bookms/web/rest/BookingRecordResource.java
@@ -1,13 +1,11 @@
 package com.bfa.bookms.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.bfa.bookms.service.BookingRecordService;
-import com.bfa.bookms.web.rest.util.HeaderUtil;
-import com.bfa.bookms.web.rest.util.PaginationUtil;
-import com.bfa.searchms.service.dto.SearchFlightDTO;
-import com.bfa.bookms.service.dto.BookingRecordDTO;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
 
-import io.swagger.annotations.ApiParam;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -15,15 +13,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import com.bfa.bookms.service.BookingRecordService;
+import com.bfa.bookms.service.dto.BookingRecordDTO;
+import com.bfa.bookms.web.rest.util.HeaderUtil;
+import com.bfa.bookms.web.rest.util.PaginationUtil;
+import com.codahale.metrics.annotation.Timed;
+
+import io.swagger.annotations.ApiParam;
 
 /**
  * REST controller for managing BookingRecord.
